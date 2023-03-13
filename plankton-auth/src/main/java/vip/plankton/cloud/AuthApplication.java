2dcb77005cbef21fbf44aa61de917305b6af0736
Wukh <823507949@qq.com>
2023-03-13 15:13:33 +0800

    add pom.xml 增加description标签描述

--- plankton-auth/src/main/java/vip/plankton/cloud/AuthApplication.java
+++ plankton-auth/src/main/java/vip/plankton/cloud/AuthApplication.java
@@ -2,6 +2,7 @@ package vip.plankton.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 统一授权认证微服务 启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Wukh
  * @create 2023-03-10
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 
--- plankton-modules/plankton-system/src/main/java/vip/plankton/cloud/SystemApplication.java
+++ plankton-modules/plankton-system/src/main/java/vip/plankton/cloud/SystemApplication.java
@@ -1,10 +1,20 @@
 package vip.plankton.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
- * 系统管理 启动类
+ * 系统模块 启动类
  *
  * @author Wukh
  * @create 2023-03-13
  */
+@EnableDiscoveryClient
+@SpringBootApplication
 public class SystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
 }
